67322d0996d7a491720166fbfea33c32e633833a
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-04-08 13:35:35 +0900

    Initial commit

---
+++ src/main/java/Doit116p.java
@@ -0,0 +1,14 @@
+public class Doit116p {
+
+    public static void main(String[] args) {
+        int dan;
+        int times;
+
+        for (dan = 2; dan < 10; dan++) {
+            for (times = 1; times < 10; times++) {
+                System.out.println(dan + "X" + times + "=" + dan * times);
+            }
+            System.out.println();
+        }
+    }
+}
